e85337d547700c2cf006b72109310e89e36b3a47
kunj <kunj.pandya@rinfochips.com>
2023-06-09 14:43:58 +0530

    malloc3 modified because of rebase

--- malloc3.c
+++ malloc3.c
@@ -14,7 +14,7 @@ int main() {
         printf("Memory allocation failed. Exiting...\n");
         return 1;
     }
-
+printf("this is what changed for rebase");
     // Read integers from the user
     printf("Enter %d integers:\n", n);
     for (int i = 0; i < n; i++) {
